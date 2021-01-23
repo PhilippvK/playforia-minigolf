a1a1fc22909113c9cc705e76fab9ff2519ce137d
PhilippvK <philipp.van-kempen@tum.de>
2021-01-24 00:58:07 +0100

    add empty com.leaningtech.client.Global implementation

---
+++ client/src/main/java/com/leaningtech/client/Global.java
@@ -0,0 +1,18 @@
+/*
+ */
+
+package com.leaningtech.client;
+
+/**
+ * This is just a dummy class
+ */
+public
+class Global {
+  public static Object jsCall(String funcName, Object... arg) {
+    return null;
+  }
+
+  public static int jsCallI(String funcName, Object... arg) {
+    return -1;
+  }
+}
