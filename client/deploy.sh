dd8d4f009a82be5eb21ba57b019419e5f03d5cf2
PhilippvK <philipp.van-kempen@tum.de>
2021-01-24 08:02:25 +0100

    fix deploy.sh

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,6 +1,8 @@
 cp -r ../assets/html/ ../public/
 cp target/client-*.jar ../public/AGolf/
 cp -r target/classes/agolf ../public/AGolf/agolf
+mkdir ../public/Shared/
+cp -r src/main/resources/sound/shared/ ../public/Shared/sound
 cd ../public/AGolf
 ../../cheerpj_2.1/cheerpjfy.py client-*.jar
 cd ..
